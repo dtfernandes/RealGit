cef29a61fa39a22795ae785f4365da55eb020bea
Daniel Fernandes <danielteotonio@sapo.pt>
2020-02-20 14:58:25 +0000

    First commit

---
+++ primeiroUnity/Assets/Rotate.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+    public float lel;
+    public bool a;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (lel < 5)
+        {
+            transform.Rotate(new Vector3(20, 20, 20));
+        }
+
+        if (!a)
+        {
+            lel += 0.5f;
+            if (lel > 12)
+            {
+                a = true;
+            }
+        }
+        else
+        {
+            lel -= 0.5f;
+            if (lel < 0.5f)
+            {
+                a = false;
+            }
+        }
+
+        transform.localScale = new Vector3(lel,transform.localScale.y,transform.localScale.z);
+
+        transform.Rotate(new Vector3(1, 1, 1));
+    }
+}
---
+++ primeiroprograma/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace primeiroprograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string A;
+
+            A = Console.ReadLine();
+            
+            for(int i = 0; i < A.Length; i++)
+            {
+                Console.WriteLine( A + " mundo!");
+            }
+
+            
+        }
+    }
+}
